13e17a0726f0ef54777c47c4ffd410088168deaa
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-04-24 21:14:44 +0200

    Initial commit

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private float parallaxSpeed;
+    private float spriteHeight;
+    private Vector3 startPos;
+   
+    void Start()
+    {
+        startPos = transform.position;
+        spriteHeight = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void Update()
+    {
+        
+    }
+}
